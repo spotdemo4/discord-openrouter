ee2ece2ae1002f7614f22ba2127badebaf4749fd
trev <me@trev.xyz>
2025-08-06 01:05:49 -0400

    bump: v0.0.3 -> v0.0.4

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
       packages = rec {
         default = pkgs.buildNpmPackage (finalAttrs: {
           pname = "discord-grok";
-          version = "0.0.3";
+          version = "0.0.4";
           src = ./.;
           nodejs = pkgs.nodejs_22;
 
