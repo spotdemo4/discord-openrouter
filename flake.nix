91fca48e1281b6113cb82ce754d24e4827594f19
trev <me@trev.xyz>
2025-08-14 11:45:20 -0400

    bump: v0.0.8 -> v0.0.9

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
       packages = rec {
         default = pkgs.buildNpmPackage (finalAttrs: {
           pname = "discord-openrouter";
-          version = "0.0.8";
+          version = "0.0.9";
           src = ./.;
           nodejs = pkgs.nodejs_22;
 
